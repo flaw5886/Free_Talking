0d2b56dd2fc320230fb2981c3c261a1f519b9af5
박진 <50200481+flaw5886@users.noreply.github.com>
2020-09-04 09:54:47 +0900

    [CREATE] iOS Project
    프로젝트 생성

---
+++ Free_Talking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Free_Talking
+//
+//  Created by 박진 on 2020/09/04.
+//  Copyright © 2020 com.parkjin.free_talking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
